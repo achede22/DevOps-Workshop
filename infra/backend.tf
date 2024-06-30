56272bc305b7aefffdce7cecbda832aaee92e279
Luis Davila <luis.davila@krimda.com>
2024-06-30 18:22:16 -0300

    local provider

--- infra/backend.tf
+++ infra/backend.tf
@@ -10,11 +10,4 @@
 
 provider "azurerm" {
   features {}
-=======
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "terraform-resource-group"
-    storage_account_name  = "spacelybackend"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
+}
